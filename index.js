f67f9d4f9311995a783552a7af6272445f628563
AveryMiller30 <averymiller30@gmail.com>
2022-05-15 11:20:53 -0500

    start of game canvas build

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+var canvasWidth = 600;
+var canvasHeight = 400;
 
+function startGame() {
+    gamesCanvsa.start();
+}
+
+var gameCanvas = {
+    canvas: document.createElement("canvas"),
+    start: function() {
+        this.canvas.width = canvasWidth;
+        this.canvas.height = canvasHeight;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+
+var player;
+var playerYPosition = 200;
+
+function startGame() {
+    gameCanvas.start();
+    player = new createPlayer(30, 30, 10);
+}
+
+function createPlayer(width, height, x) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = playerYPosition;
+    ctx = gameCanvas.context;
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}

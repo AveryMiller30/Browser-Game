aadf274369ad84d8313bfd3de107da49b24df746
AveryMiller30 <averymiller30@gmail.com>
2022-05-17 18:25:34 -0500

    new start 2.0

--- index.js
+++ index.js
@@ -1,46 +1,6 @@
-// game canvas(play area)
-var canvasWidth = 400;
-var canvasHeight = 800;
-
-function startGame() {
-    gamesCanvas.start();
-}
-
-var gameCanvas = {
-    canvas: document.createElement("canvas"),
-    start: function() {
-        this.canvas.width = canvasWidth;
-        this.canvas.height = canvasHeight;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    }
-}
-
-function startGame() {
-    gameCanvas.start();
-    
-}
-// player build and position
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
-}
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
-
-    object.addEventListener('dblclick', () => {
-        object.remove()
-    })
-}
-
-newImage('assets/gamecar.png', 20, 170)
 
 
-function moveRight() {
 
-}
+
+
+
